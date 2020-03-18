05dfb44c100e66a488a757992ae78b92e5e51e7b
Jeroen Manders <jeroen@manders.be>
2020-03-18 04:43:22 +0000

    Moved generate_ssh_config_for_vpc here

--- auto-source/env.sh
+++ auto-source/env.sh
@@ -1,1 +1,9 @@
 export AWS_COMMONS_MODULE_DIR="$(pwd)";
+
+function generate_ssh_config_for_vpc() {
+  local function_name="generate_ssh_config_for_vpc" vpc_name;
+  import_args "$@";
+  check_required_arguments "$function_name" vpc_name;
+  log_info "Generating SSH configuration for VPC into ~/.ssh/generated.d/$vpc_name.";
+  "$AWS_COMMONS_MODULE_DIR/utils/generate_vpc_ssh_config.py" "$vpc_name" > ~/.ssh/generated.d/$vpc_name;
+}
